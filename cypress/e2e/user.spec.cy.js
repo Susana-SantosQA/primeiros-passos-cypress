492430ba49a59e80c3907855518789cecff2ce5f
Susana dos Santos <criaoesfantasticas@gmail.com>
2025-07-15 22:49:53 -0300

    Add page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,16 @@
 import userData from  '../fixtures/userData.json'
+import LoginPage from '../pages/loginPage.cy'
+import DashboardPage from '../pages/dashboardPage.cy'
+import MenuPage from '../pages/menuPage.cy'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const menuPage = new MenuPage()
 
 describe('Orange HRM Tests', () => {
 
-  const selectorList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+  const selectorsList = {
+    
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField: ".oxd-input--active",
@@ -22,35 +23,35 @@ describe('Orange HRM Tests', () => {
   }
 
   it.only('User Info UpDate - Success', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userSuccess.username)
-    cy.get(selectorList.passwordField).type(userData.userSuccess.password)
-    cy.get(selectorList.loginButton).click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.dashboardGrid)
-    cy.get(selectorList.myInfoButton).click()
-    cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
-    cy.get(selectorList.lastNameField).clear().type('LastNameTest')
-    cy.get(selectorList.genericField).eq(3).clear().type('Employee')
-    cy.get(selectorList.genericField).eq(4).clear().type('OtherIdTest')
-    cy.get(selectorList.genericField).eq(5).clear().type('DriversLinceseTest')
-    cy.get(selectorList.genericField).eq(6).clear().type('2025-03-10')
-    cy.get(selectorList.dateCloseButton).click()
-    cy.get(selectorList.submitButton).eq(0).click()
-    cy.get('body').should('contain', 'Successfully Update')
-    cy.get('.oxd-toast-close')
+    loginPage.accessLoginPage()
+    loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
 
-    cy.get(selectorList.gerericComboBox).eq(0).click()
-    cy.get(selectorList.thirdItemComboBox).click()
-    cy.get(selectorList.gerericComboBox).eq(1).click()
-    cy.get(selectorList.fourthItemComboBox).click()
+    dashboardPage.checkDashboardPage()
+    
+    menuPage.accessMyInfo()
+
+    cy.get(selectorsList.firstNameField).clear().type('FirstNameTest')
+    cy.get(selectorsList.lastNameField).clear().type('LastNameTest')
+    cy.get(selectorsList.genericField).eq(3).clear().type('Employee')
+    cy.get(selectorsList.genericField).eq(4).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('DriversLinceseTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2025-03-10')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Update')
+    cy.get('.oxd-toast-close', {timeout:10000}).should('be.visible').click()
+
+    cy.get(selectorsList.gerericComboBox).eq(0).click()
+    cy.get(selectorsList.thirdItemComboBox).click()
+    cy.get(selectorsList.gerericComboBox).eq(1).click()
+    cy.get(selectorsList.fourthItemComboBox).click()
 
   })
   it('Login - fail', () => {
     cy.visit('/auth/login')
-    cy.get(selectorList.usernameField).type(userData.userFail.username)
-    cy.get(selectorList.passwordField).type(userData.userFail.password)
-    cy.get(selectorList.loginButton).click()
-    cy.get(selectorList.wrongCredentialAlert)
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.wrongCredentialAlert)
   })
 })
